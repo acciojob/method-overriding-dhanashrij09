811181081974c5005e148abee526617d0e4e0d70
Vivek Jadhav <67087656+vivekjadhav-vj@users.noreply.github.com>
2023-02-14 17:11:22 +0530

    correct code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,6 +9,7 @@ public class Main {
     }
 
     public static class B extends A{
+        @Override
         public String meth(){
             return "Method is overridden in Extended class B";
         }
@@ -18,7 +19,7 @@ public class Main {
         B b = new B ();
         b.meth();
 
-        B b1 = new B();
-        b1.meth();
+       // B b1 = new B();
+        b.meth();
     }
 }
